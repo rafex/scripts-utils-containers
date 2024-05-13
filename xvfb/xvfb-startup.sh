dcdae5332fd69cd5579527c0f17329d240788aaa
rafex <rafex@rafex.dev>
2024-05-13 13:01:30 -0600

    script para server X

---
+++ xvfb/xvfb-startup.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+Xvfb :99 -ac -screen 0 "$XVFB_RES" -nolisten tcp $XVFB_ARGS &
+XVFB_PROC=$!
+sleep 1
+export DISPLAY=:99
+"$@"
+kill $XVFB_PROC
--- xvfb/xvfb.sh
+++ xvfb/xvfb.sh
@@ -10,7 +10,8 @@ trap _kill_procs SIGTERM
 XVFB_WHD=${XVFB_WHD:-1280x720x16}
 
 # Start Xvfb
-Xvfb :99 -ac -screen 0 $XVFB_WHD -nolisten tcp &
+#Xvfb :99 -ac -screen 0 $XVFB_WHD -nolisten tcp &
+Xvfb :99 -screen 0 1024x768x24 +extension GLX +render -noreset >> xsession.log 2>&1 &
 xvfb=$!
 
 export DISPLAY=:99
